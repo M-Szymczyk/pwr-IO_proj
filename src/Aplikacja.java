5080e98aed0a49fec848aa3883887e222c37bce4
Slon09 <34111177+Slon09@users.noreply.github.com>
2020-12-09 21:52:01 +0100

    Update Aplikacja.java
    Dodano metodę dodającą model do listy modeli

--- src/Aplikacja.java
+++ src/Aplikacja.java
@@ -5,11 +5,26 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import java.util.ArrayList;
 
 public class Aplikacja {
-    ArrayList<Uzytkownik> uzytkownicy;
-    ArrayList<Kategoria> kategorie;
-    ArrayList<Model> modele;
+    static ArrayList<Uzytkownik> uzytkownicy;
+    static ArrayList<Kategoria> kategorie;
+    static ArrayList<Model> modele;
     public static void main(String[] args) {
         System.out.println("Hello");
+    }
+
+
+    public static void dodajModel(Model model){
+        for(Model x: modele){
+            if(model.getNazwa.equals(x.getNazwa())) {
+                System.out.println("Model już istnieje") ;
+                return;
+            }
+        }
+        System.out.println("Dodano pomyslnie");
+        modele.add(model);
+    }
+    public static void dodajKategorie(Kategoria k){
+        kategorie.add(k);
     }
 
 }
