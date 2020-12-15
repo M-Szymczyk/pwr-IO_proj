d7c7a16c516bf1fb7300c8badd749d8720e07ffc
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2020-12-15 16:45:49 +0100

    Add egzemplarz test, eguals, hashcode method

--- src/main/java/System/data/Egzemplarz.java
+++ src/main/java/System/data/Egzemplarz.java
@@ -1,29 +1,78 @@
 package System.data;
 
+/**
+ * Klasa przechowuje danoe o pojedynczym sprzecie wykorzystywanym w wypozyczalni
+ */
 public class Egzemplarz {
     private StanSprzetu stan_egzemplarza;
-    private Integer numer_seryjny;
+    private Integer numer_seryjny;//todo zastanowisc sie czy nie powienien tu byc string
     private Model model;
 
 
+    /**
+     * Konstruktor egzemplarza
+     * @param stan_egzemplarza obecny stan egzemplarza
+     * @param numer_seryjny numer seryjny sprzetu
+     * @param model jaki jest model sprzetu
+     */
     public Egzemplarz(StanSprzetu stan_egzemplarza, Integer numer_seryjny, Model model) {
         this.stan_egzemplarza = stan_egzemplarza;
         this.numer_seryjny = numer_seryjny;
         this.model = model;
     }
-    public void zmienStanSprzetu(StanSprzetu stan_egzemplarza){
+
+    /**
+     * aktualizacja stanu sprzetu
+     * jezeli podany argument to null, nie zostanie zmieniony stan
+     * @param stan_egzemplarza nowy stan
+     */
+    public void zmienStanSprzetu(StanSprzetu stan_egzemplarza) throws Exception {
+        if(stan_egzemplarza==null)
+            throw new Exception("Podano nulla");
         this.stan_egzemplarza=stan_egzemplarza;
     }
 
-
+    /**
+     * Zwraca obecny stan egzemplarza
+     * @return obecny stan egzemplarza
+     */
     public StanSprzetu getStan_egzemplarza() {
         return stan_egzemplarza;
     }
+
+    /**
+     * @return zwraca numer seryjny egzemplarza
+     */
     public Integer getNumer_seryjny() {
         return numer_seryjny;
     }
 
+    /**
+     * @return model egzemplarza
+     */
     public Model getModel() {
         return model;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Egzemplarz)) return false;
+
+        Egzemplarz that = (Egzemplarz) o;
+
+        if (getStan_egzemplarza() != that.getStan_egzemplarza()) return false;
+        if (!getNumer_seryjny().equals(that.getNumer_seryjny())) return false;
+        return getModel().equals(that.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getStan_egzemplarza().hashCode();
+        result = 31 * result + getNumer_seryjny().hashCode();
+        result = 31 * result + getModel().hashCode();
+        return result;
+    }
+
+
 }
---
+++ test/System/data/EgzemplarzTest.java
@@ -0,0 +1,48 @@
+package System.data;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EgzemplarzTest {
+    Model model;
+    Kategoria kat;
+    Egzemplarz eg;
+
+    @Before
+    public void setUp() {
+        kat=new Kategoria("kat","");
+        model =new Model("mod",kat);
+        eg=new Egzemplarz(StanSprzetu.DOSTEPNY,1,model);
+    }
+
+    @Test
+    public void getStan_egzemplarza() {
+        assertEquals(StanSprzetu.DOSTEPNY,eg.getStan_egzemplarza());
+    }
+
+    @Test
+    public void getNumer_seryjny() {
+        Integer one=1;
+        assertEquals( one,eg.getNumer_seryjny());
+    }
+
+    @Test
+    public void getModel() {
+        assertEquals(model,eg.getModel());
+    }
+
+    @Test(expected = Exception.class)
+    public void zmienStanSprzetu() throws Exception {
+        try {
+            eg.zmienStanSprzetu(StanSprzetu.USZKODZONY);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(StanSprzetu.USZKODZONY,eg.getStan_egzemplarza());
+        eg.zmienStanSprzetu(null);
+        assertEquals(StanSprzetu.USZKODZONY,eg.getStan_egzemplarza());
+    }
+}
