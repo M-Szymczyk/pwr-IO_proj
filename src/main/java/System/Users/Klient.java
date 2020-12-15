acf2f0fe2d3cbe7e0b9e64c7674fe9be53a2d4a9
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2020-12-15 20:13:10 +0100

    add some comments, method equals, hashcode and test some of method

--- src/main/java/System/Users/Klient.java
+++ src/main/java/System/Users/Klient.java
@@ -6,28 +6,78 @@ import System.data.Wypozyczenie;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Klasa odpowiedzialana za przechowywanie inforamcji o wypozyczeniach klienta
+ */
 public class Klient extends Uzytkownik {
-    private Integer idKlienta;
+    private final Integer idKlienta;
     private ArrayList<Wypozyczenie> wypozyczenia;
     private Double naleznoscDoZaplaty;
 
-    public void zglosZgubienieZniszczenia(Model m, Wypozyczenie w){
-
+    /**
+     * Metoda wywowywana jezeli jeden z egzemplarzy wypozyczenia zostal zniszczony/zgubiony
+     * @param w w ktorym wypozyczeniu zgubiono sprzet
+     * @param ilosc ilosc ile przedmiotow z wypozyczenia zgubiono
+     */
+    public void zglosZgubienieZniszczenia(Wypozyczenie w,Integer ilosc){
+        setNaleznoscDoZaplaty(getNaleznoscDoZaplaty()+w.getEgzemplarze().get(0).getModel().getCenaZaUszedzenia()*ilosc);
     }
-    public void wydluzWypozyczenie(Date date,Wypozyczenie wypozyczenie){
 
-
+    /**
+     * Moetoda wykorzystywana do przedluzania wypozyczenia
+     * @param date nowa data wypozyczenia
+     * @param wypozyczenie ktore wypozyczenie przedluza
+     * @throws Exception jezeli nie ma dostepnych egzemplarzy do wypozyczenia
+     */
+    public void wydluzWypozyczenie(Date date,Wypozyczenie wypozyczenie) throws Exception {
+        if(wypozyczenie.getEgzemplarze().get(0).getModel().getIlosDostepnychEgzemplarzy()<
+                 wypozyczenie.getEgzemplarze().size())
+            throw new Exception("Brak dsotepnych egzemplarzy");
+        else
+            setNaleznoscDoZaplaty(wypozyczenie.getEgzemplarze().get(0).getModel().getCenaZaDzienWypozyczenia()*
+                (date.getTime()-wypozyczenie.getData_zwrotu().getTime()));
     }
+
+    /**
+     * Metoda wypozycza sprzet
+     * @param model jaki model sprzetu wypozyczasz
+     * @param dateWyp data wypozyczenia
+     * @param dateZwrot data zwrotu
+     * @param ilosc ilosc wypozyczanego sprzetu
+     * @throws Exception jezeli nie ssa dostepne zadne sprzety
+     */
     public void wypozyczSprzet(Model model,Date dateWyp,Date dateZwrot,Integer ilosc) throws Exception {
         dodajDoWypozyczenia(new Wypozyczenie(idKlienta,dateWyp,dateZwrot,model,ilosc));
     }
+
+    /**
+     * //todo metoda do uzupelnienia
+     * Wyszukuje modele po danje nazwie???
+     * @param nazwa nazwa szukanego modelu
+     * @return referencje do modelu
+     */
     public Model wyszukajModel(String nazwa){
         return null;
     }
-    public void dodajDoWypozyczenia(Wypozyczenie w){
 
+    /**
+     * Dodaje wypozyczenie do arrayListy
+     * @param w dodawne wypozyczenie
+     */
+    public void dodajDoWypozyczenia(Wypozyczenie w){
+        wypozyczenia.add(w);
     }
 
+    /**
+     * Konstruktor
+     * @param imie imie klienta
+     * @param nazwisko nazwisko klienta
+     * @param login login klienta
+     * @param haslo haslo klienta
+     * @param email email kleinta
+     * @param user typ uzytkownika//todo na pewno to powinoo byc tez pobierane
+     * @param idKlienta id klienta //przydal by sie kostruktor z jakims przydzieleaniem id klienta
+     */
     public Klient(String imie, String nazwisko, String login, String haslo, String email, TypUzytkownika user, Integer idKlienta) {
         super(imie, nazwisko, login, haslo, email, user);
         this.idKlienta = idKlienta;
@@ -35,6 +85,7 @@ public class Klient extends Uzytkownik {
         wypozyczenia=new ArrayList<>();
     }
 
+    ///////////////////////////// Gettery i settery ////////////////////////
     public Double getNaleznoscDoZaplaty() {
         return naleznoscDoZaplaty;
     }
@@ -43,4 +94,30 @@ public class Klient extends Uzytkownik {
         this.naleznoscDoZaplaty = naleznoscDoZaplaty;
     }
 
+    public Wypozyczenie getWypozyczenie(int id) {
+        return wypozyczenia.get(id);
+    }
+    public Integer getiloscWypozyczen(){return  wypozyczenia.size();}
+
+    public Integer getIdKlienta() {
+        return idKlienta;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Klient)) return false;
+
+        Klient klient = (Klient) o;
+
+        if (!getIdKlienta().equals(klient.getIdKlienta())) return false;
+        return getNaleznoscDoZaplaty() != null ? getNaleznoscDoZaplaty().equals(klient.getNaleznoscDoZaplaty()) : klient.getNaleznoscDoZaplaty() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getIdKlienta().hashCode();
+        result = 31 * result + (getNaleznoscDoZaplaty() != null ? getNaleznoscDoZaplaty().hashCode() : 0);
+        return result;
+    }
 }
--- test/System/Users/KlientTest.java
+++ test/System/Users/KlientTest.java
@@ -1,45 +1,64 @@
 package System.Users;
 
-import System.data.Egzemplarz;
-import System.data.Kategoria;
-import System.data.Model;
-import System.data.StanSprzetu;
+import System.data.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Date;
 
+import static org.junit.Assert.*;
 
-class KlientTest {
-
-    @Test
-    void zglosZgubienieZniszczenia() {
-    }
-
-    @Test
-    void wydluzWypozyczenie() {
-    }
-
-    @Test
-    void wypozyczSprzet() {
+public class KlientTest {
+    Model model;
+    Wypozyczenie wypozyczenie;
+    Klient klient;
+    @Before
+    public void setUp() throws Exception {
         Kategoria kategoria=new Kategoria("testKat","");
-        Model model=new Model("tsetModel",kategoria);
+        model=new Model("tsetModel",kategoria);
         ArrayList<Egzemplarz> egzemplarzArrayList=new ArrayList<>();
         for(int i=0;i<5;i++)
             egzemplarzArrayList.add(new Egzemplarz(StanSprzetu.DOSTEPNY,i,model));
         model.setEgzemplarze(egzemplarzArrayList);
 
+        klient=new Klient("mikolaj","ktos","admin","admin","admin@admin.pl", TypUzytkownika.KLIENT,1);
 
-        Klient klient=new Klient("mikolaj","szymczyk","admin","admin","admin@admin.pl",TypUzytkownika.KLIENT,1);
-        try {
-            klient.wypozyczSprzet(model,new Date(1),new Date(15),5);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        wypozyczenie=new Wypozyczenie(klient.getIdKlienta(), new Date(1), new Date(15), model, 5);
     }
 
     @Test
-    void wyszukajModel() {
+    public void zglosZgubienieZniszczenia() {
+        double kasa=klient.getNaleznoscDoZaplaty();
+        klient.zglosZgubienieZniszczenia(wypozyczenie,1);
+        assertNotEquals(kasa,klient.getNaleznoscDoZaplaty());
+    }
+
+    @Test(expected = Exception.class)
+    public void zglosZgubienieZniszczenia1() {
+        double kasa=klient.getNaleznoscDoZaplaty();
+        klient.zglosZgubienieZniszczenia(wypozyczenie,100);
+    }
+    @Test(expected = Exception.class)
+    public void zglosZgubienieZniszczenia2() {
+        double kasa=klient.getNaleznoscDoZaplaty();
+        klient.zglosZgubienieZniszczenia(wypozyczenie,-1);
+    }
+
+    @Test
+    public void wydluzWypozyczenie() throws Exception {
+        Double naleznosc =klient.getNaleznoscDoZaplaty();
+        klient.wydluzWypozyczenie(new Date(20),wypozyczenie);
+        assertNotEquals(naleznosc,klient.getNaleznoscDoZaplaty());
+    }
+
+    @Test
+    public void wypozyczSprzet() throws Exception {
+        Integer iloscWypozyczen= klient.getiloscWypozyczen();
+        klient.wypozyczSprzet(model, new Date(1), new Date(15), 5);
+        //System.out.println(klient.getWypozyczenie(klient.getiloscWypozyczen()-1).equals(wypozyczenie));
+        Assert.assertNotEquals(iloscWypozyczen,klient.getiloscWypozyczen());
+
     }
 }
