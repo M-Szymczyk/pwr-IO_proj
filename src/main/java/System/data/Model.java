6b86f08aec3fdd0afa31db286c62e1182d5db1f7
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2020-12-15 17:52:15 +0100

    Add model test, eguals, hashcode method and some comments

--- src/main/java/System/data/Model.java
+++ src/main/java/System/data/Model.java
@@ -1,7 +1,11 @@
 package System.data;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
+/**
+ * Klasa przechowuje informacje o danym modelu
+ */
 public class Model {
     private ArrayList<Egzemplarz> egzemplarze;
     private Integer ilosDostepnychEgzemplarzy;
@@ -11,24 +15,38 @@ public class Model {
     private Kategoria kategoria;
     private String opis;
 
+    /**
+     * @return arrayList egzemplarzy
+     */
     public ArrayList<Egzemplarz> getEgzemplarze() {
         return egzemplarze;
     }
 
+    /**
+     * kostruktor
+     * @param nazwaModelu nazwa
+     * @param cenaZaDzienWypozyczenia cena za dzien
+     * @param cenaZaUszedzenia cena wykorzystywana w razie zniszczenia lub uszkodzenia jednego z egzemplarzy
+     * @param kategoria w jakiej kategorii jest model
+     */
     public Model(String nazwaModelu, Double cenaZaDzienWypozyczenia,
                  Double cenaZaUszedzenia, Kategoria kategoria) {
         this.egzemplarze = new ArrayList<>();
-        int ilosc=0;
-        for(Egzemplarz e :egzemplarze)
-            if(e.getStan_egzemplarza().equals(StanSprzetu.DOSTEPNY))
-                ilosc++;
-        this.ilosDostepnychEgzemplarzy = ilosc;
+
+        this.ilosDostepnychEgzemplarzy = 0;
         this.nazwaModelu = nazwaModelu;
         this.cenaZaDzienWypozyczenia = cenaZaDzienWypozyczenia;
         this.cenaZaUszedzenia = cenaZaUszedzenia;
         this.kategoria = kategoria;
         this.opis = "";
     }
+
+    /**
+     * kostruktor
+     * @param nazwaModelu nazwa
+     * @param kategoria w jakiej kategorii jest model
+     *
+     */
     public Model(String nazwaModelu, Kategoria kategoria) {
         this.egzemplarze = new ArrayList<>();
         this.ilosDostepnychEgzemplarzy = 0;
@@ -39,10 +57,19 @@ public class Model {
         this.opis = "";
     }
 
+    /**
+     * metoda usuwa pojednczy egzemplarz z listy modeli
+     * @param numerSeryjny numer seryjny modeli
+     */
     public void usunEgzemplarz(int numerSeryjny){
         egzemplarze.removeIf(e -> e.getNumer_seryjny().equals(numerSeryjny));
+        //todo pytanie czy powinno tez usuwac przypisanie modelu w egzemplarzu
     }
 
+    /**
+     * Ustawia aktualna liste egzemplarzy
+     * @param egzemplarze nowa arraylista
+     */
     public void setEgzemplarze(ArrayList<Egzemplarz> egzemplarze) {
         int ilosc=0;
         this.egzemplarze = egzemplarze;
@@ -52,6 +79,16 @@ public class Model {
         ilosDostepnychEgzemplarzy=ilosc;
     }
 
+    /**
+     * dodaje pojedynczy egzemplarz do listy
+     * @param egzemplarz dodawny egzemplarz
+     */
+    public void dodajEgzemplarz(Egzemplarz egzemplarz){
+        egzemplarze.add(egzemplarz);
+    }
+
+    /*Gettery i Settery\*/
+///////////////////////////////////////////////////////////////////////////////////////////////
     public Integer getIlosDostepnychEgzemplarzy() {
         return ilosDostepnychEgzemplarzy;
     }
@@ -98,5 +135,35 @@ public class Model {
 
     public void setOpis(String opis) {
         this.opis = opis;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Model)) return false;
+
+        Model model = (Model) o;
+
+        if (getEgzemplarze() != null ? !getEgzemplarze().equals(model.getEgzemplarze()) : model.getEgzemplarze() != null)
+            return false;
+        if (!getIlosDostepnychEgzemplarzy().equals(model.getIlosDostepnychEgzemplarzy())) return false;
+        if (!Objects.equals(nazwaModelu, model.nazwaModelu)) return false;
+        if (getCenaZaDzienWypozyczenia() != null ? !getCenaZaDzienWypozyczenia().equals(model.getCenaZaDzienWypozyczenia()) : model.getCenaZaDzienWypozyczenia() != null)
+            return false;
+        if (getCenaZaUszedzenia() != null ? !getCenaZaUszedzenia().equals(model.getCenaZaUszedzenia()) : model.getCenaZaUszedzenia() != null)
+            return false;
+        if (!getKategoria().equals(model.getKategoria())) return false;
+        return getOpis() != null ? getOpis().equals(model.getOpis()) : model.getOpis() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result =  getIlosDostepnychEgzemplarzy().hashCode();
+        result = 31 * result + (nazwaModelu != null ? nazwaModelu.hashCode() : 0);
+        result = 31 * result + (getCenaZaDzienWypozyczenia() != null ? getCenaZaDzienWypozyczenia().hashCode() : 0);
+        result = 31 * result + (getCenaZaUszedzenia() != null ? getCenaZaUszedzenia().hashCode() : 0);
+        result = 31 * result + getKategoria().hashCode();
+        result = 31 * result + (getOpis() != null ? getOpis().hashCode() : 0);
+        return result;
     }
 }
---
+++ test/System/data/ModelTest.java
@@ -0,0 +1,53 @@
+package System.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+import static org.junit.Assert.*;
+
+public class ModelTest {
+    Model mod;
+    Kategoria kat;
+    ArrayList<Egzemplarz> arrayList;
+    Egzemplarz e1,e2;
+    @Before
+    public void setUp(){
+        kat=new Kategoria("kat","");
+        mod=new Model("mod",kat);
+        arrayList=new ArrayList<>();
+        e1=new Egzemplarz(StanSprzetu.DOSTEPNY,1,mod);
+        e2=new Egzemplarz(StanSprzetu.DOSTEPNY,52,mod);
+        arrayList.add(e1);
+        arrayList.add(e2);
+        mod.setEgzemplarze(arrayList);
+    }
+
+    @Test
+    public void usunEgzemplarz() {
+        int size = arrayList.size();
+        mod.usunEgzemplarz(1);
+        assertThat(mod.getEgzemplarze(), hasItems(e2));
+        assertEquals(size-1,mod.getEgzemplarze().size());
+    }
+
+    @Test
+    public void setEgzemplarze() {
+        ArrayList<Egzemplarz> arrayList1=new ArrayList<>();
+        arrayList1.add(new Egzemplarz(StanSprzetu.NIEDOSTEPNY,2,mod));
+        mod.setEgzemplarze(arrayList1);
+        assertEquals(arrayList1,mod.getEgzemplarze());
+
+    }
+    @Test(expected = Exception.class)
+    public void setEgzemplarze2(){
+        Model mod1=new Model("mod1",kat);
+        ArrayList<Egzemplarz> arrayList2=new ArrayList<>();
+        arrayList2.add(new Egzemplarz(StanSprzetu.USZKODZONY,3,mod1));
+        mod.setEgzemplarze(arrayList2);
+        //todo wydaje mi sie ze powinno wyzucic blad jak chcesz przypisac egzemplarz innego modelu
+
+    }
+}
